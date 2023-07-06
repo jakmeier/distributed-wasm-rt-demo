91f4b54e99293c2b62fd6fefc6b641b8ae7829d9
Jakob Meier <mail@jakobmeier.ch>
2023-07-07 00:53:50 +0200

    optimize spin component

--- spin-component/src/lib.rs
+++ spin-component/src/lib.rs
@@ -11,10 +11,9 @@ use std::str::FromStr;
 fn handle_spin_component(req: Request) -> Result<Response> {
     let job = api::RenderJob::from_str(req.uri().path())?;
 
-    // println!("{job:?}");
-    // let dt = std::time::Instant::now();
+    let dt = std::time::Instant::now();
     let reponse_bytes = job.render();
-    // println!("done after {:<#.1?}", dt.elapsed());
+    println!("{job:?} done after {:<#.1?}", dt.elapsed());
 
     Ok(http::Response::builder()
         .status(200)
